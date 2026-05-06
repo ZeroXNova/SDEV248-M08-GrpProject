384f3534ebffd5a67dbda4a8a514c7a611f67ff9
James Klick <jklick@ivytech.edu>
2026-05-06 17:43:41 -0400

    Fixed the player rotation and flashlight

--- bodies/player.gd
+++ bodies/player.gd
@@ -1,8 +1,40 @@
 extends CharacterBody2D
-@onready var flash_light: PointLight2D = $flashlight
 
-var targetPosition: Vector2
+@export var speed: float = 180.0
+@export var flashlight_hold_offset := Vector2(0, 0)
+
+@onready var flashlight_pivot: Node2D = $FlashlightPivot
+@onready var beam_sprite: Sprite2D = $FlashlightPivot/BeamSprite
+
+func _ready() -> void:
+	#beam_sprite.centered = false
+#	beam_sprite.position = Vector2.ZERO
+	#beam_sprite.rotation = 0
+	return
 
 func _physics_process(delta: float) -> void:
-	if targetPosition:
-		flash_light.look_at(targetPosition)
+	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_dir * speed
+	move_and_slide()
+	
+	var mouse_pos = get_global_mouse_position()
+	var direction_to_mouse = mouse_pos - global_position
+	
+	if direction_to_mouse != Vector2.ZERO:
+		$Pivot.rotation = direction_to_mouse.angle() + deg_to_rad(175)
+	
+
+#	flashlight_pivot.position = flashlight_hold_offset
+
+	#var angle = (get_global_mouse_position() - flashlight_pivot.global_position).angle()
+	#flashlight_pivot.rotation = angle
+
+
+func _on_flashlight_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("enemies") and body.freeze == false:
+		body.freeze = true
+
+
+func _on_flashlight_area_body_exited(body: Node2D) -> void:
+	if body.is_in_group("enemies") and body.freeze == true:
+		body.freeze = false
